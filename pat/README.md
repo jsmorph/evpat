95976b300439153ac8eb0dcac6495894f28e2e02
Jamie Stephens <js@morphism.com>
2022-01-03 22:13:52 +0000

    AWS tests

--- pat/README.md
+++ pat/README.md
@@ -1,8 +1,14 @@
 ## ToDo
 
-- [ ] Test cases as JSON
-- [ ] Benchmarks
-- [ ] Array values
 - [ ] Null
 - [ ] Not
 - [ ] CIDR
+- [ ] Number comparison is supposed to be by string comparison?
+
+    > For numbers, EventBridge uses string representation. For
+    > example, 300, 300.0, and 3.0e2 are not considered equal.
+	
+- [ ] Many more test cases
+- [x] Test cases as JSON
+- [ ] Benchmarks
+- [x] Array values
--- pat/pat_test.go
+++ pat/pat_test.go
@@ -1,12 +1,25 @@
 package pat
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/eventbridge"
 )
 
 type TestCase struct {
+	// AWS indicates that this test should work with AWS
+	// EventBridge TestEventPattern.
+	//
+	// See TestAWS() below.
+	AWS bool `json:"aws,omitempty"`
+
 	Pat     interface{} `json:"pat"`
 	Msg     interface{} `json:"msg"`
 	Matches bool        `json:"matches"`
@@ -37,6 +50,75 @@ func TestBasic(t *testing.T) {
 			}
 			if got != tc.Matches {
 				t.Fatal(got)
+			}
+		})
+	}
+}
+
+func TestAWS(t *testing.T) {
+	if os.Getenv("AWS_PROFILE") == "" &&
+		os.Getenv("AWS_ACCESS_KEY_ID") == "" {
+		t.Skip("AWS access not configured")
+	}
+
+	ctx := context.Background()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		t.Fatalf("unable to load AWS SDK config, %v", err)
+	}
+
+	svc := eventbridge.NewFromConfig(cfg)
+
+	bs, err := ioutil.ReadFile("tests.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var cases []TestCase
+	if err := json.Unmarshal(bs, &cases); err != nil {
+		t.Fatal(err)
+	}
+
+	// The AWS TestEventPattern API insists on having certain
+	// top-level values.
+
+	ensure := func(msg interface{}) interface{} {
+		m, is := msg.(map[string]interface{})
+		if !is {
+			t.Fatalf("%s (%T) isn't a map", JSON(msg), msg)
+		}
+		for _, p := range []string{
+			"id", "account", "source", "region", "detail-type"} {
+			if _, have := m[p]; !have {
+				m[p] = "something"
+			}
+		}
+		m["time"] = time.Now().UTC().Format(time.RFC3339)
+		m["resources"] = []string{"a", "b"}
+
+		return m
+	}
+
+	for _, tc := range cases {
+		if !tc.AWS {
+			continue
+		}
+
+		t.Run(JSON(tc), func(t *testing.T) {
+			in := &eventbridge.TestEventPatternInput{
+				Event:        aws.String(JSON(ensure(tc.Msg))),
+				EventPattern: aws.String(JSON(tc.Pat)),
+			}
+			out, err := svc.TestEventPattern(ctx, in)
+			if err != nil {
+				if tc.Error {
+					return
+				} else {
+					t.Fatal(err)
+				}
+			}
+			if out.Result != tc.Matches {
+				t.Fatal(out.Result)
 			}
 		})
 	}
